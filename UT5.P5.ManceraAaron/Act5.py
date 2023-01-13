52b99839cc26b3ade909ae938463c9a4d650cd06
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-01-13 09:25:22 +0100

    Update Act5.py

--- UT5.P5.ManceraAaron/Act5.py
+++ UT5.P5.ManceraAaron/Act5.py
@@ -8,14 +8,19 @@ y muestre el máximo y el mínimo utilizando la función anterior.
 '''
 def calcularMaxMin (numeros):
     max=0
-    min=0
+    min=101
     for num in numeros:
         if num>max:
             max=num
         elif num<min:
             min=num
+    print(f"El máximo es: {max}")
+    print(f"El mínimo es: {min}")
 
 numeros=[]
 x=input("Escribe cuantos numeros vas a generar: ")
 for i in range (1,int(x),+1):
         numeros.append(random.randint(1,100))
+calcularMaxMin(numeros)
+#Para comprobar
+#print(numeros)
