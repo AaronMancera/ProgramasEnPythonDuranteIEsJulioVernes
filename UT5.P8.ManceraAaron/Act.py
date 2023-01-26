466d2cb2b32badf002e2609e034b464e935f9f46
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-01-26 18:52:13 +0100

    Update Act.py

--- UT5.P8.ManceraAaron/Act.py
+++ UT5.P8.ManceraAaron/Act.py
@@ -13,7 +13,7 @@ Programa
 
 '''
 path="./UT5.P8.ManceraAaron/datos/estaciones.csv"
-
+#Mostrar todos los datos de 43 en 43. Es decir, mostrará los 43 primeros y si elegimos la opción siguiente, mostrará los siguientes 43. Dando en total 6 páginas máximas
 def mostrarDatosPaginados():
     cont=int(43)
     print("Cargando fichero en una página con 43 estaciones...")
@@ -33,14 +33,17 @@ def mostrarDatosPaginados():
                     break
     print()
 
+#Mostrar todos los nombre y espacios de cada estación de forma ordenada por los espacios (slots)
 def mostarOrdenados():
     cont=int(43)
     print("Cargando fichero en una página con 43 ordenado...")
     with open(path, encoding='utf-8') as file:
         lector = csv.DictReader(file)
         registros=[]
+        #Esta parte recoge los datos y los guarda en la lista registro
         for registro in lector:
             #name,slots,empty_slots,free_bikes,latitude,longitude
+            #Se crea una tupla para añadirla de manera ordenada a la lista
             name = registro['name']
             slots = registro['slots']
             empty_slots = registro['empty_slots']
@@ -49,6 +52,7 @@ def mostarOrdenados():
             longitude = registro['longitude']
             tupla=(name,slots,empty_slots,free_bikes,latitude,longitude)
             registros.append(tupla)
+    #nueva lista que sera la ordenada y la que mostraremos
     sort_registros=sorted(registros, key=lambda item: item[1], reverse=True)
     for r in sort_registros:
         print(f"{r[0]} : {r[1]}")
@@ -63,6 +67,7 @@ def mostarOrdenados():
                 break
     print()
 
+#Mostrar todas las estaciones que estén con bicicletas libres (free_bikes)
 def mostrarTodasConBiciletasLibres():
     cont=int(43)
     print("Cargando fichero en una página con 43 estaciones...")
@@ -70,7 +75,8 @@ def mostrarTodasConBiciletasLibres():
         lector = csv.reader(file)
         next(lector)
         for line in lector:
-            if(int(line[3])>0):
+            #Con este condicional podemos saber si hay bicis libres o no 
+            if int(line[3])>0:
                 print(line[0])
                 cont-=1
                 if cont==0:
@@ -83,6 +89,7 @@ def mostrarTodasConBiciletasLibres():
                         break
     print()
 
+#Mostrar todas las estaciones que haya sitio para dejar la bicicleta (empty_slots)
 def mostrarTodasConEspaciosLibres():
     cont=int(43)
     print("Cargando fichero en una página con 43 estaciones...")
@@ -90,7 +97,8 @@ def mostrarTodasConEspaciosLibres():
         lector = csv.reader(file)
         next(lector)
         for line in lector:
-            if(int(line[2])>0):
+            #Con este condicional sabemos si tienen huecos para dejar la bici
+            if int(line[2])>0:
                 print(line[0])
                 cont-=1
                 if cont==0:
@@ -103,21 +111,20 @@ def mostrarTodasConEspaciosLibres():
                         break
     print()
 
+#Búsqueda de estaciones por nombre (name)
 def buscarEstacion():
     encontrado=False
-    resultado=[]
     buscar=str(input("Escribe el nombre de la estacion para buscar: "))
     print("Cargando fichero y buscando...")
     with open(path,encoding='utf-8') as file:
         lector = csv.reader(file)
         next(lector)
         for linea in lector:
+            #Este condicional comprueba que los nombres coincida con el que buscas
             if buscar.casefold() in linea[0].casefold():
-                resultado=linea
+                print(linea)
                 encontrado=True
-    if encontrado==True:
-        print(resultado)
-    else:
+    if encontrado==False:
         print("Nombre de la estación no encontrado...")
 
 opcion=int(0)
@@ -126,7 +133,7 @@ while opcion!=6:
     print("|   1. Mostrar los datos paginado                            |")
     print("|   2. Mostar ordenado por espacios                          |")
     print("|   3. Mostrar todas las estaciones con bicicletas           |")
-    print("|   4. Mostrar todas las estaciones espacio para bicicletas  |")
+    print("|   4. Mostrar todas las estaciones/espacio para bicicletas  |")
     print("|   5. Busqueda de estaciones por nombre                     |")
     print("|   6. Salir                                                 |")
     print("--------------------------------------------------------------")
